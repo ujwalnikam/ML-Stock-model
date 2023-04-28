7739fc46585f642e281141401dcf08105ae96cba
Ujwal Nikam <69692475+ujwalnikam@users.noreply.github.com>
2023-04-28 18:39:31 +0530

    Add files via upload

---
+++ pages/About Me.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# st.title("About Us")
+# st.text('PBL Grp ID 10 ')
+# st.text('Guide : Dr. Rushali Deshmukh')
+# st.text('Members: ')
+# st.text("1. Prachi Jadhav")
+# st.text("2. Rohan Jawle")
+# st.text("3. Ujwal Nikam")
+# st.text("4. Dhanashri Patil")
+# st.text("5. Sakshi Shelar")
+
+st.text("Hello Jarvis I'm Ujwal Nikam , more coder less debugger ;")
+st.text("I'm passionate about AI,ML,Data Science and also Galaxy :)")
+st.text("Let's Meet on Mars with meta !")
